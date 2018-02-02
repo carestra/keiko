46755b510b22ec3f3b9d5dd25734a620e68514da
Carlos Estrada <carestra@europe.com>
2018-02-02 21:39:28 +0100

    initial 2018

---
+++ js/keiko.js
@@ -0,0 +1,9 @@
+// Activates Tooltips for Social Links
+$('.tooltip-social').tooltip({
+  selector: "a[data-toggle=tooltip]"
+})
+
+// Activates the Carousel
+$('.carousel').carousel({
+  interval: 5000
+})
